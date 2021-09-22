4af79adc329961fd705f4802d096e4fc48739308
poposh7406 <pothenskylouis02@gmail.com>
2021-09-22 14:49:00 -0400

    toiture de la maison

--- javaScript.js
+++ javaScript.js
@@ -98,6 +98,20 @@ var c = document.getElementById("mon_canavas");
         ctx.strokeStyle="000";
         ctx.fill();
            ctx.closePath();
+
+           ctx.beginPath();
+           ctx.moveTo(350,400);
+           ctx.lineTo(500,500);
+           ctx.lineTo(500,700);
+           ctx.lineTo(200,700);
+           ctx.lineTo(200,500);
+           ctx.lineTo(350,400);
+           ctx.lineWidth=2;
+           ctx.stroke();
+           ctx.fillStyle="#ffd66b";
+           ctx.closePath();
+           ctx.strokeStyle="bleu";
+           ctx.fill();
         
         
                       
